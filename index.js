8423fe8fe7ea6ecd2bd3a01b5256c1490d7dafcd
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-01-30 20:00:56 +1100

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let writeCards = (names, event) => {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  };
+
+let countDown = (num) => {
+    let i = num;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  };
